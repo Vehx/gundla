78b1639f97e7d790348a66f30864f7ce6a11b353
Jesper <jesper.lundqvist@gmail.com>
2020-09-01 15:24:55 +0200

    Added event schema

---
+++ studio/schemas/event.js
@@ -0,0 +1,37 @@
+export default {
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'image',
+      title: 'image',
+      type: 'image',
+      options: { hotspot: true },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'date',
+      title: 'Date',
+      type: 'datetime',
+    },
+  ],
+};
