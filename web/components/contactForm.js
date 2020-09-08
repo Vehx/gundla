19e4638f12a6bd80fd7899077214f62eb729222d
vehx <delta222@gmail.com>
2020-09-08 15:22:33 +0200

    changed form to use to send email

--- web/components/contactForm.js
+++ web/components/contactForm.js
@@ -1,44 +1,47 @@
-import { useForm } from "react-hook-form";
-import { useRef } from 'react';
-import emailjs from 'emailjs-com';
+import {useState} from "react";
 
 export const ContactForm = () => {
-  const { handleSubmit, register, errors } = useForm();
-  const formEl = useRef(null);
-  const onSubmit = () => sendEmail();
+const [status, setStatus] = useState("")
+const sendEmail = (ev) => {
 
-  const sendEmail = () => {
-    emailjs.sendForm('service_6g7o6l1', 'template_sbgbmzl', formEl.current, 'user_nT7oObagtDFioyBOrj7EO')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        setStatus("SUCCESS");
+      } else {
+        setStatus("ERROR");
+      }
+    };
+    xhr.send(data);
   }
 
+
   return (
-    <form ref={formEl} onSubmit={handleSubmit(onSubmit)}>
-      <input
-        name="email"
-        ref={register({
-          required: "Required",
-          pattern: {
-            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-            message: "invalid email address"
-          }
-        })}
-      />
-      {errors.email && errors.email.message}
-
-      <input
-        name="username"
-        ref={register({
-          validate: value => value !== "admin" || "Nice try!"
-        })}
-      />
-      {errors.username && errors.username.message}
-
-      <button type="submit">Submit</button>
-    </form>
+    <form
+        onSubmit={sendEmail}
+        action="https://formspree.io/mgenglzb"
+        method="POST"
+      >
+        <label htmlFor="name">Namn/Organisation:</label>
+        <input type="text" name="name" autoComplete="off" required/>
+        <label htmlFor="telephone">Telefonnummer:</label>
+        <input type="text" name="telephone" autoComplete="off" required/>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" autoComplete="off" required/>
+        <label htmlFor="message">Meddelande:</label>
+        <input type="text" name="message" autoComplete="off" required/>
+        {status === "SUCCESS" ? <p>Tack! Ditt meddelande är skickat.</p> : <button>Skicka</button>}
+        {status === "ERROR" && <p>Åååånej! Något gick fel.</p>}
+      </form>
   );
 };
+
+
+
