47e855f783cc4738db929bee523cc7d27609b578
Jesper <jesper.lundqvist@gmail.com>
2020-09-10 14:30:01 +0200

    Optimized images

---
+++ web/components/insatagramImage.js
@@ -0,0 +1,16 @@
+export const InstagramImage = ({ src, alt }) => {
+  return (
+    <img
+      lazyloading="true"
+      srcSet={`
+        ${src.thumbnail_resources[0].src} 150w,
+        ${src.thumbnail_resources[1].src} 240w,
+        ${src.thumbnail_resources[2].src} 320w,
+        ${src.thumbnail_resources[3].src} 480w,
+        ${src.thumbnail_resources[4].src} 640w,
+        `}
+      src={src.thumbnail_resources[0].src}
+      alt={alt}
+    />
+  );
+};
--- web/pages/index.js
+++ web/pages/index.js
@@ -2,16 +2,16 @@ import client from "../client";
 import groq from "groq";
 import { Layout } from "../components/layout";
 import { EventHero } from "../components/eventHero";
+import { InstagramImage } from "../components/insatagramImage";
 
 const Index = (props) => {
-  const instagramImages = props.pictures.map((picture) => {
-    return (
-      <img
-        key={picture.node.thumbnail_resources[4].src}
-        src={picture.node.thumbnail_resources[4].src}
-      />
-    );
-  });
+  const instagramImages = props.pictures.map((picture) => (
+    <InstagramImage
+      key={picture.node.id}
+      src={picture.node}
+      alt={picture.node.accessibility_caption}
+    />
+  ));
 
   return (
     <Layout>
@@ -22,19 +22,18 @@ const Index = (props) => {
         endTime={props.sanity.endTime}
         shortDescription={props.sanity.shortDescription}
       />
-      {instagramImages}
+      <a href="https://www.instagram.com/gundlagardscafe">{instagramImages}</a>
     </Layout>
   );
 };
 
 Index.getInitialProps = async function (context) {
-
   const { slug = "" } = context.query;
   const query = groq`*[_type == "event"][0]`;
   const sanity = await client.fetch(query, { slug });
 
   const baseUrl = "https://www.instagram.com";
-  const profileName = "yrgo_webbutvecklare";
+  const profileName = "gundlagardscafe";
   const profileUrl = `${baseUrl}/${profileName}`;
   const jsonDataUrl = `${profileUrl}/?__a=1`;
   const response = await fetch(jsonDataUrl);
