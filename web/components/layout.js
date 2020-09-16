e4e1ab2da806462032d7c55b60d59c65799d216a
Jesper <jesper.lundqvist@gmail.com>
2020-09-16 18:08:08 +0200

    Added css for footer, hero and instagram

--- web/components/layout.js
+++ web/components/layout.js
@@ -15,7 +15,7 @@ export const Layout = (props) => (
     <div className="container">{props.children}</div>
     <Footer content={props.footer} />
     <style jsx global>{`
-      @import url("https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap");
+      @import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
 
       * {
         margin: 0;
@@ -26,20 +26,26 @@ export const Layout = (props) => (
       :root {
         --heading-font: "Amatic SC", cursive;
         --paragraph-font: "PT Sans", sans-serif;
+        --color-vanilla: #fff9e3;
+        --color-sunny: #e7ac0f;
+        --color-chocolate: #ac794d;
+        --color-black: #000;
+        --color-white: #fff;
         --padding-x: 20px;
         --padding-y: 11px;
       }
 
-      body {
-        font-family: var(--paragraph-font);
-      }
 
       p {
         line-height: 21px;
+        font-family: var(--paragraph-font);
+        font-size: 16px;
+        margin-bottom: 10px;
       }
 
       a {
         text-decoration: underline;
+        font-family: var(--paragraph-font);
         color: hotpink;
       }
 
@@ -48,11 +54,14 @@ export const Layout = (props) => (
       h3 {
         font-family: var(--heading-font);
         font-weight: bold;
+        text-transform: uppercase;
+        letter-spacing: 0.05em;
       }
 
       h3 {
         font-size: 18px;
         line-height: 23px;
+        margin-bottom: 10px;
       }
       }
       h2 {
@@ -60,32 +69,29 @@ export const Layout = (props) => (
         line-height: 30px;
       }
       h1 {
-        font-size: 36px;
+        font-size: 24px;
         line-height: 45px;
       }
 
-      .button-s {
-        font-size: 16px;
-        line-height: 21px;
-        color: #fff;
-        background-color: #000;
-        width: 264px;
-        height: 25px;
+      button {
+        font-family: var(--paragraph-font);
+        border: none;
+        cursor: pointer;
       }
-      .button-m {
+
+      .button-form {
         font-size: 18px;
-        line-height: 21px;
-        color: #000;
-        background-color: light-grey;
-        width: 315px;
-        height: 50px;
-        font-weight: bold;
-      }
-      .button-l {
-        width: 225px;
+        line-height: 23px;
+        color: var(--color-white);
+        background-color: var(--color-sunny);
+        width: 305px;
         height: 41px;
-        color: #fff;
+        text-align: center;
+        border-radius: 33px;
+        box-shadow: inset 1px 2px 4px rgba(255, 255, 255, 0.22);
+        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
       }
+
      {/* REMOVE THIS */}
       img {
         width: 100px;
