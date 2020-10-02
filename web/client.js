be58b9a904d8c402082ba40cb882abcf5fc1e15c
vehx <delta222@gmail.com>
2020-10-02 11:12:07 +0200

    fixed formatting

--- web/client.js
+++ web/client.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client";
 
 export default sanityClient({
-    projectId: "aar5m15o", // you can find this in sanity.json
-    dataset: "production", // or the name you chose in step 1
-    useCdn: true, // `false` if you want to ensure fresh data
+  projectId: "aar5m15o", // you can find this in sanity.json
+  dataset: "production", // or the name you chose in step 1
+  useCdn: true, // `false` if you want to ensure fresh data
 });
